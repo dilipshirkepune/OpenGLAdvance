74b6b305e5d75e9da0a2ec2131c6e30bfeacfdda
Dilip Shirke <dilipshirke.pune@gmail.com>
2025-03-01 15:44:25 +0000

    shaders with uniform and output color
    Added shader class to read shader source from file, compile and create shader program.
    Also have example for uniforms

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -39,6 +39,48 @@ int main()
         return -1;
     }
 
+    // Define the vertex buffer, buffer object, array buffer, bind buffer to array and upload data to GPU memory.
+
+    float vertices[] = {
+        //Position          //Color
+      0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top right
+      0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
+     -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // bottom left
+     -0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f // top left 
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+   
+    // build and compile our shader program
+    // ------------------------------------
+    Shader ourShader("shaders/vertexshader.vs", "shaders/fragmentshader.fs"); // you can name your shader files however you like
+        
     // render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -46,12 +88,39 @@ int main()
         processInput(window);
 
         // rendering commands here
-        glClearColor(1.0, 0.5, 0.0, 1.0);
+        glClearColor(1.0, 1.0, 1.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
+        //glUseProgram(shaderProgram);
+        ourShader.use();
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        //glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        
+        // Wireframe
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        //solid fill
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         // check and call events and swap the buffers
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    //glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 
